afc4e6d986ddc5199705233d1632e721945a3edf
马新才 <maxincai@126.com>
2021-09-03 22:43:46 +0800

    [代码优化](master): 使用标准 PSR 事件替代 EventHandler

--- api-server/app/core/work-contact/src/Listener/Tag/DeleteTagRawListener.php
+++ api-server/app/core/work-contact/src/Listener/Tag/DeleteTagRawListener.php
@@ -8,23 +8,29 @@ declare(strict_types=1);
  * @contact  group@mo.chat
  * @license  https://github.com/mochat-cloud/mochat/blob/master/LICENSE
  */
-namespace MoChat\App\WorkContact\QueueService\Tag;
+
+namespace MoChat\App\WorkContact\Listener\Tag;
 
 use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\Di\Annotation\Inject;
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Event\Annotation\Listener;
 use MoChat\App\Corp\Contract\CorpContract;
 use MoChat\App\Corp\Logic\AppTrait;
 use MoChat\App\WorkContact\Contract\WorkContactTagContract;
 use MoChat\App\WorkContact\Contract\WorkContactTagGroupContract;
 use MoChat\App\WorkContact\Contract\WorkContactTagPivotContract;
+use MoChat\App\WorkContact\Event\Tag\DeleteTagRawEvent;
 use MoChat\Framework\Constants\ErrorCode;
 use MoChat\Framework\Exception\CommonException;
+use Psr\Container\ContainerInterface;
 
 /**
- * 企业微信 删除客户标签回调
- * Class DestroyCallBackApply.
+ * 删除标签事件监听器
+ *
+ * @Listener()
  */
-class DestroyCallBackApply
+class DeleteTagRawListener implements ListenerInterface
 {
     use AppTrait;
 
@@ -40,36 +46,53 @@ class DestroyCallBackApply
      */
     private $corp;
 
-    public function handle(array $wxResponse): void
+    /**
+     * @Inject()
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function listen(): array
     {
+        return [
+            DeleteTagRawEvent::class
+        ];
+    }
+
+    /**
+     * @param DeleteTagRawEvent $event
+     */
+    public function process(object $event)
+    {
+        $message = $event->message;
         //查询企业id
-        $this->corp = $this->getCorp($wxResponse['ToUserName']);
+        $this->corp = $this->getCorp($message['ToUserName']);
         if (empty($this->corp)) {
             return;
         }
 
         //若删除的是标签
-        if ($wxResponse['TagType'] == 'tag') {
-            $this->handleTag($wxResponse);
+        if ($message['TagType'] == 'tag') {
+            $this->handleTag($message);
         }
         //若删除的是标签组
-        if ($wxResponse['TagType'] == 'tag_group') {
-            $this->handleTagGroup($wxResponse);
+        if ($message['TagType'] == 'tag_group') {
+            $this->handleTagGroup($message);
         }
     }
 
     /**
      * 若删除的是标签组.
-     * @param $wxResponse
+     * @param $message
      */
-    private function handleTagGroup($wxResponse)
+    private function handleTagGroup($message)
     {
-        $tagGroup               = make(WorkContactTagGroupContract::class);
-        $tag                    = make(WorkContactTagContract::class);
+        $tagGroup = make(WorkContactTagGroupContract::class);
+        $tag = make(WorkContactTagContract::class);
         $contactTagPivotService = make(WorkContactTagPivotContract::class);
 
         //查询本地有无此分组
-        $tagGroupInfo = $tagGroup->getWorkContactTagGroupByWxGroupId($wxResponse['Id'], ['id']);
+        $tagGroupInfo = $tagGroup->getWorkContactTagGroupByWxGroupId($message['Id'], ['id']);
         if (empty($tagGroupInfo)) {
             return;
         }
@@ -83,52 +106,52 @@ class DestroyCallBackApply
         $tagIds = array_column($tagInfo, 'id');
 
         //删除分组
-        $tagGroupRes = $tagGroup->deleteWorkContactTagGroup((int) $tagGroupInfo['id']);
-        if (! is_int($tagGroupRes)) {
-            $this->logger->error('删除标签分组回调失败', $wxResponse);
+        $tagGroupRes = $tagGroup->deleteWorkContactTagGroup((int)$tagGroupInfo['id']);
+        if (!is_int($tagGroupRes)) {
+            $this->logger->error('删除标签分组回调失败', $message);
             throw new CommonException(ErrorCode::SERVER_ERROR, '删除标签分组失败');
         }
 
         //删除分组下的标签
         $tagRes = $tag->deleteWorkContactTags($tagIds);
-        if (! is_int($tagRes)) {
-            $this->logger->error('删除标签分组回调失败', $wxResponse);
+        if (!is_int($tagRes)) {
+            $this->logger->error('删除标签分组回调失败', $message);
             throw new CommonException(ErrorCode::SERVER_ERROR, '删除标签失败');
         }
         //删除客户标签
         $contactTag = $contactTagPivotService->deleteWorkContactTagPivotsByTagId($tagIds);
-        if (! is_int($contactTag)) {
+        if (!is_int($contactTag)) {
             throw new CommonException(ErrorCode::SERVER_ERROR, '客户标签删除失败');
         }
     }
 
     /**
      * 若删除的是标签.
-     * @param $wxResponse
+     * @param $message
      */
-    private function handleTag($wxResponse)
+    private function handleTag($message)
     {
-        $tag                    = make(WorkContactTagContract::class);
+        $tag = make(WorkContactTagContract::class);
         $contactTagPivotService = make(WorkContactTagPivotContract::class);
 
         //查询标签信息
-        $tagInfo = $tag->getWorkContactTagsByWxTagId($wxResponse['Id'], ['id']);
+        $tagInfo = $tag->getWorkContactTagsByWxTagId($message['Id'], ['id']);
 
         if (empty($tagInfo)) {
             return;
         }
 
         //删除标签
-        $tagRes = $tag->deleteWorkContactTag((int) $tagInfo['id']);
+        $tagRes = $tag->deleteWorkContactTag((int)$tagInfo['id']);
 
-        if (! is_int($tagRes)) {
-            $this->logger->error('删除标签回调失败', $wxResponse);
+        if (!is_int($tagRes)) {
+            $this->logger->error('删除标签回调失败', $message);
             throw new CommonException(ErrorCode::SERVER_ERROR, '删除标签失败');
         }
 
         //删除客户标签
         $contactTag = $contactTagPivotService->deleteWorkContactTagPivotsByTagId([$tagInfo['id']]);
-        if (! is_int($contactTag)) {
+        if (!is_int($contactTag)) {
             throw new CommonException(ErrorCode::SERVER_ERROR, '客户标签删除失败');
         }
     }
